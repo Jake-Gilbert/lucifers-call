5312417268a3b2ef9a2fc4e8b65830bce1607990
Zainab Parvin <z_parvin@outlook.com>
2019-03-10 22:45:15 +0000

    Removed the old inventory and code

--- TeamProject/Player/Scripts/Player.gd
+++ TeamProject/Player/Scripts/Player.gd
@@ -29,12 +29,6 @@ func _physics_process(delta):
 		switch_animation("idle")
 	
 	level = get_tree().get_current_scene().get_filename()
-	
-#	if (Input.is_action_pressed("ui_inventory") && inventory == false):
-#		get_tree().paused = true
-#		get_tree().change_scene("res://inventoryUI/Scenes/Scene_PlayerInventory.tscn")
-#	else:
-#		get_tree().paused = false
 
 #Function that says if this key pressed then player should move in this direction
 func controls_loop():
