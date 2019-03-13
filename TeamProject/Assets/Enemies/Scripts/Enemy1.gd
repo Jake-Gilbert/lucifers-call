0f38358091e836057d3222477961a224fcec02da
Faly Richou <richouf@aston.ac.uk>
2019-03-13 12:49:23 +0000

    Cleaned up combat

--- TeamProject/Assets/Enemies/Scripts/Enemy1.gd
+++ TeamProject/Assets/Enemies/Scripts/Enemy1.gd
@@ -1,13 +1,12 @@
 #Code for enemies
 #Features enemy death
 #Created by Jake Gilbert
-extends KinematicBody2D
+extends Area2D
 
 # class member variables go here, for example:
 # var a = 2
-const GRAVITY = 10
-const SPEED = 30
-const FLOOR = Vector2(0, -1)
+var location = 0
+var speed = 6
 var health = 4
 
 var alive = true 
@@ -16,20 +15,21 @@ var velocity = Vector2()
 var direction = 1
 	# Called when the node is added to thse scene for the first time.
 	# Initialization here
-func dead():
-	alive = false
-	$Sprite.hide()
-	velocity = Vector2(0, 0)
-func getHealth():
-	return health
-func setHealth():
-	health = health - 1
-func _physics_process(delta):
-	velocity.x   = SPEED * direction
-	velocity.y += GRAVITY
-	velocity = move_and_slide(velocity, FLOOR)
-	
-	if is_on_wall():
-			direction = direction * -1
+func _ready():
+	set_physics_process(true)
 
-			
+func lose_health():
+	if (health != 0):
+		health = health -1
+		if (health == 0):
+			queue_free()
+
+func _physics_process(delta):
+	location += speed
+	get_parent().set_offset(location)
+	var bodies = get_overlapping_bodies()
+	for body in bodies:
+		if body.name == "Player":
+			body.lose_health()
+
+
---
+++ TeamProject/UI/Scenes/DialogueBox.gd
@@ -0,0 +1,12 @@
+extends NinePatchRect
+
+var textToPrint
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+ func _clearText():
+	get_child().set_text
+	
