b22de83b3e32eb3c4e39eff9870419f250ca0cd5
hnarme <hnarme@ymail.com>
2019-03-16 15:55:46 +0000

    Revert "New changes"
    This reverts commit c54ebe523a4d1dcae43f8269862d72f47d55cbf2.

--- TeamProject/Global_SceneSwitch.gd
+++ TeamProject/Global_SceneSwitch.gd
@@ -2,6 +2,11 @@ extends Node
 
 var packed_scene = PackedScene.new()
 
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
 func save_current_scene():
 	packed_scene.pack(get_tree().get_current_scene())
 	ResourceSaver.save("res://my_scene.tscn", packed_scene)
@@ -11,3 +16,8 @@ func reload_last_saved():
 	
 	var my_scene = packed_scene.instance()
 	add_child(my_scene)
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
