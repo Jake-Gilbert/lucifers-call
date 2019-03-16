029717c507b2f96294b4e65967ebcffd29d13e40
Jake <Jakegilbert@blueyonder.co.uk>
2019-03-16 16:37:38 +0000

    Revert "Settings"

--- TeamProject/Assets/Enemies/Scripts/Enemy1.gd
+++ TeamProject/Assets/Enemies/Scripts/Enemy1.gd
@@ -27,7 +27,7 @@ func lose_health():
 func _physics_process(delta):
 	location += speed
 	get_parent().set_offset(location)
-	var bodies = get_overlapping_areas()
+	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Player":
 			body.lose_health()
