6bf6ef51226a27abb1b78ffa57e3ef9f382f927c
hnarme <hnarme@ymail.com>
2019-03-17 10:56:34 +0000

    merge

--- TeamProject/Assets/Scripts/Key.gd
+++ TeamProject/Assets/Scripts/Key.gd
@@ -11,10 +11,7 @@ func _physics_process(delta):
 		if body.name == "Player" && body.get("items") < 9:
 			body.items += 1
 			Global_Player.inventory_addItem("1")
-<<<<<<< HEAD
 			hide()
 			Global_SceneSwitch.save_current_scene()
-=======
->>>>>>> 98dd228996154472d1679b7c53c491dfe9646e8e
 			queue_free()
 	pass
