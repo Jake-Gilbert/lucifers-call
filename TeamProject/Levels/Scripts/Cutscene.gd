73fadabbcaf141009980e6e1738ff8bce0975e88
Zainab Parvin <z_parvin@outlook.com>
2019-03-19 15:38:34 +0000

    Implemented boss room cut scenes

---
+++ TeamProject/Levels/Scripts/Cutscene.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	pass
+
+
+func _on_Cutscene_body_entered(body):
+	if body.name == "Player":
+		pass
+	pass
