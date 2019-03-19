3fb8f22b0a649885d939aeb9c375e5cb1d03386c
FalyRichou <47387075+FalyRichou@users.noreply.github.com>
2019-03-19 16:03:49 +0000

    Revert "Deleted unnecessary code"

--- TeamProject/Player/Scripts/Player.gd
+++ TeamProject/Player/Scripts/Player.gd
@@ -154,6 +154,63 @@ func switch_animation(animation):
 	if $animChar.current_animation != newAnim:
 		$animChar.play(newAnim)
 
+func get_state():
+	var save_dict = {
+		pos = {
+			x = position.x,
+			y = position.y
+		},
+		speed = SPEED,
+		moveDirection = movedir,
+		spriteDirection = spritedir,
+		level = level
+	}
+	return save_dict
+
+func load_state(data):
+	for attribute in data:
+		if attribute == "pos":
+			position(Vector2(data["pos"]["x"], data["pos"]["y"]))
+		else:
+			set(attribute, data[attribute])
+
+func _on_SaveBtn_pressed():
+	var save_game = File.new()
+	save_game.open("user://savegame.save", File.WRITE)
+	var save_nodes = get_tree().get_nodes_in_group("Persist")
+	for i in save_nodes:
+		var node_data = i.call("save");
+		save_game.store_line(to_json(node_data))
+	save_game.close()
+
+func _on_LoadBtn_pressed():
+    var save_game = File.new()
+    if not save_game.file_exists("user://save_game.save"):
+        return # Error! We don't have a save to load.
+
+    # We need to revert the game state so we're not cloning objects during loading. This will vary wildly depending on the needs of a project, so take care with this step.
+    # For our example, we will accomplish this by deleting savable objects.
+    var save_nodes = get_tree().get_nodes_in_group("Persist")
+    for i in save_nodes:
+        i.queue_free()
+
+    # Load the file line by line and process that dictionary to restore the object it represents
+    save_game.open("user://savegame.save", File.READ)
+    while not save_game.eof_reached():
+        var current_line = parse_json(save_game.get_line())
+        # First we need to create the object and add it to the tree and set its position.
+        var new_object = load(current_line["filename"]).instance()
+        get_node(current_line["parent"]).add_child(new_object)
+        new_object.position = Vector2(current_line["pos_x"], current_line["pos_y"])
+        # Now we set the remaining variables.
+        for i in current_line.keys():
+            if i == "filename" or i == "parent" or i == "pos_x" or i == "pos_y":
+                continue
+            new_object.set(i, current_line[i])
+    save_game.close()
+	
+
+
 
 func _on_invinciblityTimer_timeout():
 	get_node("Sprite").set_modulate(default)
@@ -169,3 +226,4 @@ func _on_Door_body_entered(body):
 		L1Map.goto_scene("res://UI/Instructions/PuzzleOneInstruction.tscn")
 	else:
 		pass
+
